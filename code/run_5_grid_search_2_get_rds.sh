aa177b2aef18e83e70cdf606adbf1801fdc7759a
Ryan Brill <ryguy123@berkeley.edu>
2022-04-25 09:59:10 -0400

    try grid_search_1a on HPCC

---
+++ code/run_5_grid_search_2_get_rds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mv ./gs2_100_2.rds    ../../Dropbox/HPCC/gs2_100_2.rds  
+mv ./gs2_100_4.rds    ../../Dropbox/HPCC/gs2_100_4.rds  
+mv ./gs2_100_5.rds    ../../Dropbox/HPCC/gs2_100_5.rds  
+mv ./gs2_100_7.rds    ../../Dropbox/HPCC/gs2_100_7.rds  
+mv ./gs2_100_10.rds   ../../Dropbox/HPCC/gs2_100_10.rds  
+boxup
+mv ../../Dropbox/HPCC/gs2_100_2.rds    ./gs2_100_2.rds  
+mv ../../Dropbox/HPCC/gs2_100_4.rds    ./gs2_100_4.rds  
+mv ../../Dropbox/HPCC/gs2_100_5.rds    ./gs2_100_5.rds  
+mv ../../Dropbox/HPCC/gs2_100_7.rds    ./gs2_100_7.rds  
+mv ../../Dropbox/HPCC/gs2_100_10.rds   ./gs2_100_10.rds  
