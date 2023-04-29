4c2028c741e5a83f3471d68798343ab3707b49ae
Ryan Brill <ryansbrill@gmail.com>
2023-04-28 22:46:01 -0400

    b11 eMaxESPNScore df v2 AJ

--- code_2/b11_smm_eMaxWeightedScore_df.R
+++ code_2/b11_smm_eMaxWeightedScore_df.R
@@ -52,13 +52,16 @@ if (version_ == 1) {
 } else {
   stop("this version_ has not yet been implemented")
 }
-idxs_lower = nrow(GRID)/num_folds_parralelization_ * (fold_-1)
-idxs_upper = nrow(GRID)/num_folds_parralelization_ * fold_
-GRID_OG = GRID_OG[floor(idxs_lower):ceiling(idxs_upper),]
-GRID = GRID[floor(idxs_lower):ceiling(idxs_upper),]
+df_folds = tibble(fold = cut(1:nrow(GRID), breaks=num_folds_parralelization_,labels=FALSE)) %>%
+  mutate(i = row_number()) %>%
+  filter(fold == fold_)
+idx_lower = df_folds$i[1]
+idx_upper = df_folds$i[2]
+c(idx_lower, idx_upper)
+GRID_OG = GRID_OG[idx_lower:idx_upper,]
+GRID = GRID[idx_lower:idx_upper,]
 GRID_OG
 GRID
-
 
 results = matrix(nrow=nrow(GRID), ncol=length(ns))
 colnames(results) = paste0("n=",ns)
