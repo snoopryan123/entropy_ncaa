6aaa111c023df95fcef0ab627b191731cdccadd1
Ryan Brill <ryansbrill@gmail.com>
2023-05-01 15:34:10 -0700

    b11 wp testHamming

--- code_2/b11_smm_wpWeightedScore_df_aggregation.R
+++ code_2/b11_smm_wpWeightedScore_df_aggregation.R
@@ -16,10 +16,10 @@ if (version_ == 1) {
 
 GRID = tibble()
 for (fold_ in 1:num_folds_parralelization_) {
-  df_fold = read_csv(paste0(output_folder,"plot_grid_wpScore_v",version_,"_fold",fold_,".csv"))
+  df_fold = read_csv(paste0(output_folder,"plot_grid_wpMaxScore_v",version_,"_fold",fold_,".csv"))
   GRID = bind_rows(GRID, df_fold)
 }
-write_csv(GRID, paste0(output_folder,"plot_grid_wpScore_v",version_,".csv"))
+write_csv(GRID, paste0(output_folder,"plot_grid_wpMaxScore_v",version_,".csv"))
 
 
 
