b8a1500cd895ebe6a37ad5e314ca8ec447d6907c
Ryan Brill <ryansbrill@gmail.com>
2023-05-01 15:35:06 -0700

    b11 wp testHamming

--- code_2/run_b11_smm_eMaxWeightedScore_df_boxup.sh
+++ code_2/run_b11_smm_eMaxWeightedScore_df_boxup.sh
@@ -3,6 +3,8 @@
 
 mv plot_11/plot_grid_eMaxScore_v1.csv			../../Dropbox/HPCC/.
 mv plot_11/plot_grid_eMaxScore_v2.csv			../../Dropbox/HPCC/.
+mv plot_11/plot_grid_wpMaxScore_v1.csv			../../Dropbox/HPCC/.
+mv plot_11/plot_grid_wpMaxScore_v2.csv			../../Dropbox/HPCC/.
 boxup
 mv ../../Dropbox/HPCC/plot_grid_eMaxScore_*		plot_11/.
 
