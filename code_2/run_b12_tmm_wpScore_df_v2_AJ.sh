e539626adee5bc7c06b434a081142a6f8974e504
Ryan Brill <ryansbrill@gmail.com>
2023-05-03 07:55:24 -0700

    b12 tmm wpMaxScore_df run

---
+++ code_2/run_b12_tmm_wpScore_df_v2_AJ.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+#$ -N b12_tmm_wpScore_df_v2
+#$ -j y
+#$ -m e -M ryguy123@sas.upenn.edu 
+#$ -o job_output/$JOB_NAME-$JOB_ID.log
+
+## MORE RAM
+#$ -l m_mem_free=5G
+
+## ARRAY JOB
+#$ -t 1-10
+#$ -o job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
+
+Rscript --vanilla b12_tmm_wpScore_df.R 2 ${SGE_TASK_ID}
+
+
+
+
