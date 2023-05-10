82ad15378cc30440fe66ebdc2fae504821a1ce4c
Ryan Brill <ryansbrill@gmail.com>
2023-05-10 10:30:50 -0700

    run_b13_fmm_eMaxScore_df_v1_AJ

--- code_2/b13_fmm_entropyTail_WP_df.R
+++ code_2/b13_fmm_entropyTail_WP_df.R
@@ -1,217 +1,130 @@
 
 source("a2_main.R")
 
-######################################################################
-### To maximize your probability of winning the bracket challenge, ###
-### given the "true" known win probabilities P,                    ###
-### given that the opponent submits k naively chalky brackets,     ###
-### and given that you submit n brackets, from what entropy range  ###
-### should you submit brackets?                                    ###
-######################################################################
+output_folder = "./plot_13/"
+
+entropy_tailMap_filename = paste0(output_folder, "df_fmm_entropy_tail_map.csv")
+if (file.exists(entropy_tailMap_filename)) {
+  entropy_tailMap = read_csv(entropy_tailMap_filename)
+} else {
+  stop("first, run the file `b13_fmm_entropyTail_map.R` to get `df_fmm_entropy_tail_map.csv` ")
+}
+
+############################
+### Grids to Search Over ###
+############################
 
 ### grid to search over
-GRID = expand.grid(
+GRID1 = expand.grid(
+  h = sort(unique(entropy_tailMap$h)), ### how chalky we are
+  opp_prob_method = c("naive_chalky"), ### how chalky they are
   n = 10^(2:4),
-  k = 10^(2:4),
-  opp_prob_method = c("naive_chalky", "P_538_2022", "naive_random", "super_chalky")
+  k = 10^(2:4)
 ) %>%
   filter(n <= k) %>%
   # filter(n < k) %>% 
   arrange(n,k)
-GRID
+GRID1
 
 ######################################################################
-### To maximize your probability of winning the bracket challenge, ###
+### To maximize your expected score of in the bracket challenge,   ###
 ### given the "true" known win probabilities P,                    ###
-### given that the opponent submits k naively chalky brackets,     ###
 ### and given that you submit n brackets, from what entropy range  ###
 ### should you submit brackets?                                    ###
 ######################################################################
 
-args = commandArgs(trailingOnly=TRUE)
-GRID_ROW_IDX = as.numeric(args[1])
-PARAMS = GRID[GRID_ROW_IDX,]
+# version_ = 1
+# fold_ = 1
+# num_folds_ = 1
 
-if (max(PARAMS$n, PARAMS$k) <= 10000) {
-  NUM_RUNS = 150 #500
+args = commandArgs(trailingOnly=TRUE)
+version_ = as.numeric(args[1]) ### which GRID to search over
+fold_ = as.numeric(args[2])
+num_folds_ = as.numeric(args[3])
+
+### get GRID
+if (version_ == 1) {
+  GRID_OG = GRID1
+} else {
+  stop("this `version_` has not yet been implemented")
+}
+
+### map h in GRID_OG to (left or right tail, entropy cutoff)
+entropy_tailMap_1 = entropy_tailMap %>% rename(entropy_cutoff = h, mean_entropy = H) #%>% mutate(H_rounded = round(H))
+GRID_OG_A = GRID_OG %>% 
+  fuzzyjoin::difference_left_join(entropy_tailMap_1, max_dist = Inf, 
+                                  distance_col = "dist", by=c("h" = "mean_entropy")) %>%
+  group_by(n,k,h) %>%
+  arrange(n,k,h,dist) %>%
+  slice_head() %>%
+  ungroup() %>% select(-c(mean_entropy, dist))
+GRID_OG_A
+
+### get GRID from this fold
+if (num_folds_ > 1) {
+  df_folds_0 = tibble(fold = cut(1:nrow(GRID_OG_A), breaks=num_folds_, labels=FALSE)) %>% mutate(i = row_number()) 
+} else {
+  df_folds_0 = tibble(fold = 1, i = 1:nrow(GRID_OG_A))
+}
+df_folds = df_folds_0 %>% filter(fold == fold_)
+idxs = df_folds$i
+print(paste0("idxs ", idxs))
+GRID = GRID_OG_A[idxs,]
+GRID
+
+#### keep the runtime down...
+if (max(GRID$n) <= 10000) {
+  NUM_RUNS = 100 #500
 } else {
   NUM_RUNS = 5 #FIXME
 }
 
-df_entropy_conversion = read_csv("df_entropy_conversion.csv")
-
-H_stars_espn = numeric(NUM_RUNS)
-H_stars_num_correct = numeric(NUM_RUNS)
-# hL_stars = numeric(NUM_RUNS)
+wpESPN_scores = matrix(nrow=NUM_RUNS, ncol=nrow(GRID))
+rownames(wpESPN_scores) = paste0("RUN=",1:NUM_RUNS)
+colnames(wpESPN_scores) = paste0("i=",1:nrow(GRID))
+wpHamming_scores = wpESPN_scores
 for (RUN in 1:NUM_RUNS) {
-  ### true_backets
-  set.seed(423942347+RUN*2) 
-  true_backets = sample_n_brackets(n=250)
+  set.seed(423942347+RUN*2) ### use RUN to get a different random sample
   
-  if (PARAMS$opp_prob_method == "super_chalky") {
-    opp_submitted_backets = sample_n_brackets_entropyRange(n=PARAMS$k, hL=-Inf, hU=45.75)
-  } else {
-    opp_submitted_backets = sample_n_brackets(n=PARAMS$k, prob_method = PARAMS$opp_prob_method)
-  }
-  opp_scores_espn = compute_max_score(opp_submitted_backets, true_backets, "ESPN")
-  opp_scores_num_correct = compute_max_score(opp_submitted_backets, true_backets, "num_correct")
+  true_backets = sample_n_brackets(n=250) ### true_backets
   
-  ### find best (-inf, hU] or [hL, -inf)
-  wp_hU_espn = numeric(nrow(df_entropy_conversion))
-  wp_hU_num_correct = wp_hU_espn
-  for (i in 1:nrow(df_entropy_conversion)) {
-    print(paste0("run = ", RUN, " of ", NUM_RUNS, ", i = ", i, " of ", nrow(df_entropy_conversion)))
+  ### find the expected max score from sampling n brackets from (-inf, hU] or [hL, -inf)
+  for (i in 1:nrow(GRID)) {
+    print(paste0("run = ", RUN, " of ", NUM_RUNS, ", i = ", i, " of ", nrow(GRID)))
     
-    if (df_entropy_conversion$left_tail[i]) {
-      ### sample brackets from left tail of entropy distribution
-      hL = -Inf
-      hU = df_entropy_conversion$h[i]
+    ### sample our n brackets from the given entropy range
+    n = GRID$n[i]
+    hL = if (GRID$left_tail[i]) -Inf else GRID$entropy_cutoff[i]
+    hU = if (GRID$left_tail[i]) GRID$entropy_cutoff[i] else Inf
+    our_submitted_backets = sample_n_brackets_entropyRange(n, hL, hU, prob_method="P_538_2022") 
+    
+    ### sample opponents' k brackets
+    k = GRID$k[i]
+    opp_prob_method = as.character(GRID$opp_prob_method[i])
+    if (opp_prob_method == "super_chalky") {
+      opp_submitted_backets = sample_n_brackets_entropyRange(n=k, hL=-Inf, hU=45.75)
     } else {
-      ### sample brackets from right tail of entropy distribution
-      hL = df_entropy_conversion$h[i]
-      hU = Inf
+      opp_submitted_backets = sample_n_brackets(n=k, prob_method = opp_prob_method)
     }
-    our_submitted_backets = sample_n_brackets_entropyRange(PARAMS$n, hL, hU, prob_method="P_538_2022") 
-    scores_espn = compute_max_score(our_submitted_backets, true_backets, "ESPN")
-    scores_num_correct = compute_max_score(our_submitted_backets, true_backets, "num_correct")
-    wp_hU_espn[i] = mean(scores_espn >= opp_scores_espn)
-    wp_hU_num_correct[i] = mean(scores_num_correct >= opp_scores_num_correct)
+    
+    ### compute the bracket scores
+    our_scores_ESPN    = compute_max_score(our_submitted_backets, true_backets, "ESPN")
+    our_scores_Hamming = compute_max_score(our_submitted_backets, true_backets, "num_correct")
+    opp_scores_ESPN    = compute_max_score(opp_submitted_backets, true_backets, "ESPN")
+    opp_scores_Hamming = compute_max_score(opp_submitted_backets, true_backets, "num_correct")
+    
+    ### compute the win proportions
+    wpESPN_scores[RUN,i]    = mean(our_scores_ESPN >= opp_scores_ESPN)
+    wpHamming_scores[RUN,i] = mean(our_scores_Hamming >= opp_scores_Hamming)
   }
-  H_star_espn = df_entropy_conversion$H[first(which(wp_hU_espn == max(wp_hU_espn)))]
-  H_star_num_correct = df_entropy_conversion$H[first(which(wp_hU_num_correct == max(wp_hU_num_correct)))]
-  print(H_star_espn); print(H_star_num_correct);
-  H_stars_espn[RUN] = H_star_espn
-  H_stars_num_correct[RUN] = H_star_num_correct
 }
 
-######
-H_star = tibble(H_stars_espn, H_stars_num_correct) %>%
-  summarise(
-    H_star_espn = mean(H_stars_espn),
-    H_star_num_correct = mean(H_stars_num_correct)
-  )
-H_star = bind_cols(PARAMS, H_star)
-write_csv(H_star, paste0("dfs/df_H_star_i",GRID_ROW_IDX,".csv"))
+### add the results to GRID
+GRID$wpESPN_scores = colMeans(wpESPN_scores)
+GRID$wpHamming_scores = colMeans(wpHamming_scores)
+print(GRID)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # hU_stars = numeric(NUM_RUNS)
-# hU_stars_espn = numeric(NUM_RUNS)
-# hU_stars_num_correct = numeric(NUM_RUNS)
-# # hL_stars = numeric(NUM_RUNS)
-# for (RUN in 1:NUM_RUNS) {
-#   ### true_backets
-#   set.seed(423942347+RUN*2) 
-#   true_backets = sample_n_brackets(n=250)
-#   
-#   if (PARAMS$opp_prob_method == "super_chalky") {
-#     opp_submitted_backets = sample_n_brackets_entropyRange(n=PARAMS$k, hL=-Inf, hU=45.75)
-#   } else {
-#     opp_submitted_backets = sample_n_brackets(n=PARAMS$k, prob_method = PARAMS$opp_prob_method)
-#   }
-#   opp_scores_espn = compute_max_score(opp_submitted_backets, true_backets, "ESPN")
-#   opp_scores_num_correct = compute_max_score(opp_submitted_backets, true_backets, "num_correct")
-#   
-#   ### find best hU
-#   GRID_hU = 38:58
-#   #### scores_hU = numeric(length(GRID_hU))
-#   # wp_hU = numeric(length(GRID_hU))
-#   # names(wp_hU) = GRID_hU
-#   wp_hU_espn = numeric(length(GRID_hU))
-#   names(wp_hU_espn) = GRID_hU
-#   wp_hU_num_correct = wp_hU_espn
-#   for (i in 1:length(GRID_hU)) {
-#     hU = GRID_hU[i]
-#     print(paste0("run = ", RUN, " of ", NUM_RUNS, ", i = ", i, " of ", length(GRID_hU), ", and hU = ", hU))
-#     our_submitted_backets = sample_n_brackets_entropyRange(PARAMS$n, -Inf, hU, prob_method="P_538_2022") 
-#     scores_espn = compute_max_score(our_submitted_backets, true_backets, "ESPN")
-#     scores_num_correct = compute_max_score(our_submitted_backets, true_backets, "num_correct")
-#     wp_hU_espn[i] = mean(scores_espn >= opp_scores_espn)
-#     wp_hU_num_correct[i] = mean(scores_num_correct >= opp_scores_num_correct)
-#   }
-#   hU_star_espn = GRID_hU[first(which(wp_hU_espn == max(wp_hU_espn)))]
-#   hU_star_num_correct = GRID_hU[first(which(wp_hU_num_correct == max(wp_hU_num_correct)))]
-#   print(hU_star_espn); print(hU_star_num_correct);
-#   hU_stars_espn[RUN] = hU_star_espn
-#   hU_stars_num_correct[RUN] = hU_star_num_correct
-#   
-#   # ### given hU, find best hL
-#   # hdiff = 2
-#   # GRID_hL = c(-Inf, 38:(hU_star - hdiff))
-#   # if (hU_star - hdiff > 38) {
-#   #   wp_hL = numeric(length(GRID_hL))
-#   #   names(wp_hL) = GRID_hL
-#   #   for (i in 1:length(GRID_hL)) {
-#   #     hL = GRID_hL[i]
-#   #     print(paste0("run = ", RUN, " of ", NUM_RUNS, ", i = ", i, " of ", length(GRID_hL), ", and hL = ", hL))
-#   #     our_submitted_backets = sample_n_brackets_entropyRange(PARAMS$n, hL, hU_star, prob_method="P_538_2022") 
-#   #     #### scores_hL[i] = compute_max_score(our_submitted_backets, true_backets, scoring_method=PARAMS$scoring_method)
-#   #     our_scores = compute_max_score(our_submitted_backets, true_backets, scoring_method=PARAMS$scoring_method)
-#   #     wp_hL[i] = mean(our_scores >= opp_scores)
-#   #   }
-#   #   hL_star = GRID_hL[last(which(wp_hL == max(wp_hL)))]
-#   # } else {
-#   #   hL_star = -Inf
-#   # }
-#   # wp_hL
-#   # 
-#   # print(c(hL_star,hU_star))
-#   
-#   # hL_stars[RUN] = hL_star
-#   # hU_stars[RUN] = hU_star
-# }
-# 
-# ######
-# h_star = 
-#   tibble(hU_stars_espn, hU_stars_num_correct) %>%
-#   summarise(
-#     hU_star_espn = mean(hU_stars_espn),
-#     hU_star_num_correct = mean(hU_stars_num_correct)
-#   )
-# h_star = bind_cols(PARAMS, h_star)
-# write_csv(h_star, paste0("dfs/df_h_star_i",GRID_ROW_IDX,".csv"))
-
-
-
-
-
-
-
-
-
-# ######
-# h_star = hU_stars %>% summarise(hU_star = mean(hU_stars))
-# h_star
-# write_csv(h_star, paste0("dfs/df_h_star_i",GRID_ROW_IDX,".csv"))
-
-# ######
-# h_stars = tibble(hL_stars, hU_stars)
-# h_stars
-#  
-# h_star = h_stars %>%
-#   mutate(hL_stars = ifelse(hL_stars == -Inf, 30, hL_stars)) %>%
-#   summarise(hL_star = mean(hL_stars),
-#             hU_star = mean(hU_stars))
-# h_star
-# write_csv(h_star, paste0("dfs/df_h_star_i",GRID_ROW_IDX,".csv"))
-
-
-
-
-
-# min(compute_entropies(sample_n_brackets(n=1e5,keep_probs = T)))
+### save GRID
+write_csv(GRID, paste0(output_folder, "plot_grid_wp_fmm_v",version_,"_fold_",fold_,".csv"))
 
 
---
+++ code_2/run_b13_fmm_boxup.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+mv plot_13/plot_grid_eMaxScore_fmm_v1_fold_1.csv			../../Dropbox/HPCC/.
+boxup
+mv ../../Dropbox/HPCC/plot_grid_eMaxScore_fmm_v1_fold_1.csv		plot_13/.
+
+
+
+
+
+
+
+ 
