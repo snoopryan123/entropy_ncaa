eff0d544c2a7bae742f02df3895affad1a63a382
Ryan Brill <ryansbrill@gmail.com>
2023-05-11 11:02:39 -0700

    run_b13_fmm_wp_df_v1_AJ

--- code_2/run_b13_fmm_boxup.sh
+++ code_2/run_b13_fmm_boxup.sh
@@ -2,8 +2,10 @@
 #!/bin/bash
 
 mv plot_13/plot_grid_eMaxScore_fmm_v1_fold_1.csv			../../Dropbox/HPCC/.
+mv plot_13/plot_grid_wp_fmm_v1_fold_1.csv			../../Dropbox/HPCC/.
 boxup
 mv ../../Dropbox/HPCC/plot_grid_eMaxScore_fmm_v1_fold_1.csv		plot_13/.
+mv ../../Dropbox/HPCC/plot_grid_wp_fmm_v1_fold_1.csv		plot_13/.
 
 
 
